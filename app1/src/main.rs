1456a1da0ef694ec675205df80ead79506f3fabd
Arielberz <aaariel.m.b@icloud.com>
2025-12-25 12:58:01 +0200

    Add

--- app1/src/main.rs
+++ app1/src/main.rs
@@ -1,5 +1,18 @@
 fn main() {
     let numbers = vec![1, 2, 3, 4, 5, 6];
     let sum: i32 = numbers.iter().sum();
-    println!("Sum = {}", sum);
+    //println!("Sum = {}", sum);
+
+    pub struct Car {
+        pub color: String,
+        pub fast: int
+}
+    fn increase_speed(car: &mut Car, amount: int) {
+        car.fast += amount;
+    }
+
+    fn print_car_info(car: &Car) {
+        println!("Car color: {}, Speed: {}", car.color, car.fast);
+    }
+
 }
