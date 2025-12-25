f184ae4c11a46dda056abf9e2a7ae3783f5721ce
Arielberz <aaariel.m.b@icloud.com>
2025-12-25 13:02:35 +0200

    Add

--- app1/src/main.rs
+++ app1/src/main.rs
@@ -5,14 +5,20 @@ fn main() {
 
     pub struct Car {
         pub color: String,
-        pub fast: int
+        pub fast: i32
 }
-    fn increase_speed(car: &mut Car, amount: int) {
+    fn increase_speed(car: &mut Car, amount: i32) {
         car.fast += amount;
     }
 
     fn print_car_info(car: &Car) {
         println!("Car color: {}, Speed: {}", car.color, car.fast);
     }
+    let mut car = Car {
+        color: String::from("Red"),
+        fast: 100,
+    };
+    increase_speed(&mut car, 50);
+    print_car_info(&car);
 
 }
